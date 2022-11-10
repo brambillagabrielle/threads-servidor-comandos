562dada47c911ca7966175292392d50b277f3572
gabrielle <gabriellebrambillacc@gmail.com>
2022-11-10 17:53:09 -0300

    exercicio-sockets

---
+++ src/ServidorComandos/ClienteServidor.java
@@ -0,0 +1,88 @@
+package ServidorComandos;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class ClienteServidor extends Thread {
+    
+    private static boolean terminado = false;
+    private final Socket conexao;
+
+    public ClienteServidor(Socket conexao) {
+        this.conexao = conexao;
+    }
+    
+    @Override
+    public void run() {
+        
+        try {
+        
+            BufferedReader entrada = new BufferedReader(
+                    new InputStreamReader(
+                          conexao.getInputStream()
+                    )
+            );
+                
+            String linha;
+            while (true) {
+                
+                linha = entrada.readLine();
+                
+                if (linha == null) {
+                    System.out.println("Fim da conexão");
+                    break;
+                }
+                
+                System.out.println(linha);
+                
+            }
+            
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+        
+        terminado = true;
+        
+    }
+    
+    public static void main(String args[]) {
+        
+        try {
+            
+            Socket conexao = new Socket("127.0.0.1", 1234);
+
+            PrintStream saida = new PrintStream(
+                    conexao.getOutputStream()
+            );
+            BufferedReader teclado = new BufferedReader(
+                    new InputStreamReader(System.in)
+            );
+            
+            System.out.println("# ");
+            
+            Thread thread = new ClienteServidor(conexao);
+            thread.start();
+            
+            String linha;
+            while(true) {
+                
+                linha = teclado.readLine();
+                
+                if(terminado) {
+                    break;
+                }
+                
+                saida.println(linha);
+                
+            }
+            
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+        
+    }
+    
+}
---
+++ src/ServidorComandos/Servidor.java
@@ -0,0 +1,116 @@
+package ServidorComandos;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Servidor extends Thread {
+    
+    private final Cliente cliente;
+    private Socket conexao;
+    private static List<Cliente> clientes;
+
+    public Servidor(Cliente cliente) {
+        this.cliente = cliente;
+    }
+    
+    @Override
+    public void run() {
+        
+        try {
+            
+            BufferedReader entrada = new BufferedReader(
+                    new InputStreamReader(
+                            cliente.getSocket().getInputStream()
+            ));
+            PrintStream saida = new PrintStream(
+                    cliente.getSocket().getOutputStream()
+            );
+            
+            String linha = entrada.readLine();
+            while (linha != null && !(linha.trim().equals(""))) {
+                
+                executaComando(linha, saida);
+                saida.println("# ");
+                linha = entrada.readLine();
+                
+            }
+            
+            clientes.remove(saida);
+            conexao.close();
+            
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+        
+    }
+    
+    public void executaComando(String comando, PrintStream saida) {
+        
+        try {
+            
+            Process processo = Runtime.getRuntime().exec(comando);
+            StringBuilder saidaComando = new StringBuilder(); 
+
+            BufferedReader leitor = new BufferedReader(
+                    new InputStreamReader(processo.getInputStream())
+            );
+
+            String linha;
+            while((linha = leitor.readLine()) != null) {
+                saidaComando.append(linha).append("\n");
+            }
+            
+            int exitVal = processo.waitFor();
+            if (exitVal == 0) {
+                System.out.println("Comando executado com sucesso");
+                saida.println(saidaComando);
+            } else {
+                saida.println("Erro");
+            }
+            
+        } catch(InterruptedException | IOException e) {
+            System.out.println(e);
+        }
+        
+    }
+    
+    public static void main(String args[]) {
+        
+        System.out.println("Servidor para execução de comandos");
+        
+        clientes = new ArrayList<>();
+        
+        try {
+            
+            ServerSocket serverSocket = new ServerSocket(1234);
+            
+            while(true) {
+                
+                Socket conexao = serverSocket.accept();
+                
+                Cliente cliente = new Cliente();
+                cliente.setIp(conexao.getRemoteSocketAddress().toString());
+                cliente.setSocket(conexao);
+                clientes.add(cliente);
+                
+                System.out.println("\nNovo usuário conectado: " + cliente.getIp());
+                System.out.println("Total de usuários atuais: " + clientes.size() + "\n");
+                
+                Thread thread = new Servidor(cliente);
+                thread.start();
+                
+            }
+            
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+        
+    }
+    
+}
